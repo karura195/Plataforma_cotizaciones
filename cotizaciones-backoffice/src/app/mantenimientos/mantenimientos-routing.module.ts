2a3b7b1571c6f1d83d1b54998994561eedf8f177
Ruben <ruben.cadenas@itland.pe>
2022-03-28 17:29:40 -0500

    se agrega la navegacion a las paginas de mantenimiento de servicios

--- cotizaciones-backoffice/src/app/mantenimientos/mantenimientos-routing.module.ts
+++ cotizaciones-backoffice/src/app/mantenimientos/mantenimientos-routing.module.ts
@@ -6,45 +6,57 @@ import { UsuarioEditarPage } from './pages/usuario-editar/usuario-editar.page';
 import { ProductoEditarPage } from './pages/producto-editar/producto-editar.page';
 import { ServicioEditarPage } from './pages/servicio-editar/servicio-editar.page';
 import { ContainerInsideComponent } from '../shared/components/container-inside/container-inside.component';
+import { ProductoListadoPage } from './pages/producto-listado/producto-listado.page';
+import { ServicioListadoPage } from './pages/servicio-listado/servicio-listado.page';
 
 
 export const MANTENIMIENTOS_COMPONENTS = [
-    UsuarioListadoPage,
-    UsuarioEditarPage,
-    ProductoEditarPage,
-    ServicioEditarPage
+	UsuarioListadoPage,
+	UsuarioEditarPage,
+	ProductoListadoPage,
+	ProductoEditarPage,
+	ServicioEditarPage,
+	ServicioListadoPage
 ]
 
 const routes: Routes = 
 [
-    {
-        path:'mantenimientos',
-        component: ContainerInsideComponent,
-        children:
-        [
-            {
-                path:'usuario/listado',
-                component: UsuarioListadoPage
-            },
-            {
-                path:'usuario/editar/:id',
-                component: UsuarioEditarPage
-            },
-            {
-                path:'producto/editar/:id',
-                component: ProductoEditarPage
-            },
-            {
-                path:'servicio/editar/:id',
-                component: ServicioEditarPage
-            }
-        ]
-    }
+	{
+		path:'mantenimientos',
+		component: ContainerInsideComponent,
+		children:
+		[
+			{
+				path:'usuario/listado',
+				component: UsuarioListadoPage
+			},
+			{
+				path:'usuario/editar/:id',
+				component: UsuarioEditarPage
+			},
+			{
+				path:'producto/listado',
+				component: ProductoListadoPage
+			},
+			{
+				path:'producto/editar/:id',
+				component: ProductoEditarPage
+			},
+			{
+				path:'servicio/listado',
+				component: ServicioListadoPage
+			},
+			{
+				path:'servicio/editar/:id',
+				component: ServicioEditarPage
+			}
+		]
+	}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  declarations: []
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
+	declarations: []
 })
 export class MantenimientosRoutingModule { }
--- cotizaciones-backoffice/src/app/mantenimientos/mantenimientos.module.ts
+++ cotizaciones-backoffice/src/app/mantenimientos/mantenimientos.module.ts
@@ -6,22 +6,26 @@ import { SharedModule } from '../shared/shared.module';
 
 import { MantenimientosRoutingModule, MANTENIMIENTOS_COMPONENTS } from "./mantenimientos-routing.module";
 import { UsuarioService } from './services/usuario.service';
+import { ServicioService } from './services/servicio.service';
+import { ProductoService } from './services/producto.service';
 
 
 @NgModule({
-    imports: 
-    [
-        CommonModule,
+	imports: 
+	[
+		CommonModule,
 		FormsModule,
 		SharedModule,
-        MantenimientosRoutingModule
-    ],
-    providers:[
-        UsuarioService
-    ],
-    declarations: 
-    [
-        MANTENIMIENTOS_COMPONENTS
-    ]
+		MantenimientosRoutingModule
+	],
+	providers:[
+		UsuarioService,
+		ProductoService,
+		ServicioService
+	],
+	declarations: 
+	[
+		MANTENIMIENTOS_COMPONENTS
+	]
 })
 export class MantenimientosModule { }
